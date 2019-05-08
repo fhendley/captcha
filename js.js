8bcca5179721a162f5ceb3e69aa5023599ee8c16
Francesca Hendley <49951431+fhendley@users.noreply.github.com>
2019-05-08 20:38:08 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,22 +1,55 @@
 // selects random words from list to present as the captcha
+// var[] image;
+// for(int i=6;i<9;i++){
+//   image[i] = "images/sb" + i + ".PNG";
+// }
+//
+// console.log('image);
 
-function captcha() {
-  words = ['PROSTHETICS', 'BEAST', 'ORTHOTIST', 'MEDICAL', 'PHYSICOTHERAPY', 'PAEDIATRIC', 'PROTHETIST' ];
- randomWord = words[Math.floor(Math.random()*words.length)];
-document.getElementById("contactForm").innerHTML = randomWord;
+
+// function captcha() {
+
+var random_images_array = ["prosthetics.PNG", "beast.PNG", "medical.PNG", "orthotist.PNG", "physiotherapy.PNG", "paediatric.PNG" ];
+var word;
+function captcha(imgAr, path) {
+    path = path || 'images/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = (path + imgAr[ num ]);
+    // var imgStr = '<img src="' + path + img + '>';
+    // name = imgStr.getAttribute('alt');
+    // document.write(imgStr); document.close();
+    // console.log(imgAR)
+    document.getElementById("contactForm").src = img;
+    // console.log(imgStr);
+    console.log(img);
+    word = img.slice (7, -4);
+    console.log(word);
+
+
+
+
+  //   words = ['PROSTHETICS', 'BEAST', 'ORTHOTIST', 'MEDICAL', 'PHYSICOTHERAPY', 'PAEDIATRIC', 'PROTHETIST' ];
+  //  randomWord = words[Math.floor(Math.random()*words.length)];
+  // document.getElementById("contactForm").innerHTML = randomWord;
+
 };
+
 
 
 // Creates validation rules for form
 function validate()
 {
-    if((form.name.value == "") || (form.email.value == "") || (form.phone.value == "") || (form.mes.value == "") || (form.captcha.value == ""))
+var local = word;
+
+    // if((form.name.value == "") || (form.email.value == "") || (form.phone.value == "") || (form.mes.value == "") || (form.captcha.value == ""))
+    if(form.name.value == "")
     {
-        alert("Please fill all fields");
+        alert("Please fill in all fields");
         return false;
     }
-    else if(form.captcha.value !== randomWord)
+    else if(form.captcha.value !== word)
     {
+
         alert("Please enter the correct Captcha code");
         return false;
     }
@@ -25,4 +58,20 @@ function validate()
       alert("Submitted");
         return true;
     }
-}
+};
+
+
+
+
+
+
+
+
+//
+//   randomWord = image[Math.floor(Math.random()*image.length)];
+//   console.log(randomWord);
+
+//
+//
+// console.log(randomWord);
+// };
